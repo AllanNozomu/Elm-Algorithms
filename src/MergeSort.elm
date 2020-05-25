2cf15815d1b9683bc99de846e64c092e75684883
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2020-05-25 09:38:17 -0300

    Change split method to two pointer approach

--- src/MergeSort.elm
+++ src/MergeSort.elm
@@ -15,16 +15,23 @@ merge l1 l2 =
             else
                 e2 :: merge (e1 :: r1) r2
 
+halfsplit : List a -> (List a, List a)
+halfsplit l =
+    let
+        halsplitAux : List a -> List a -> (List a, List a)
+        halsplitAux x y = 
+            case (x,y) of
+                (xs::xr, _::_::yr) -> halsplitAux xr yr |> Tuple.mapFirst ((::) xs)
+                (xs, _) -> ([], xs)
+    in
+    halsplitAux l l
+
 split : List comparable -> List comparable
 split l =
     case l of
         [] -> []
         a :: [] -> [ a ]
-        _ ->
-            let
-                left = 
-                    List.take (List.length l // 2) l |> split
-                right =
-                    List.drop (List.length l // 2) l |> split
-            in
-            merge left right
+        _ ->  
+            case halfsplit l |> Tuple.mapBoth split split of 
+            (x,y) ->  merge x y
+
