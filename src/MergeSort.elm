b5184c6bbefaf6f9947b823331dc85a9db22e078
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2020-06-02 20:25:56 -0300

    Change function to better name

--- src/MergeSort.elm
+++ src/MergeSort.elm
@@ -43,22 +43,22 @@ mergeSortSteps i l =
             case (left, right) |> Tuple.mapBoth (mergeSortSteps i) (mergeSortSteps (i + List.length left)) of 
             ((sortedLeft, resLeft, sequenceLeft), (sortedRight, resRight, sequenceRight)) ->
                 let
-                    (sortedL, res, sequence) = mergeOnlyOne sortedLeft sortedRight i (i + List.length left + 1)
+                    (sortedL, res, sequence) = mergeSteps sortedLeft sortedRight i (i + List.length left + 1)
                 in  
                     (sortedL, 
                     List.map (\ele -> ele ++ right) resLeft ++ List.map (\ele -> sortedLeft ++ ele) resRight ++ res,
                     sequenceLeft ++ sequenceRight ++ sequence
                     )
 
-mergeOnlyOne : List comparable -> List comparable -> Int -> Int -> (List comparable, List (List comparable), List (Int, Int))
-mergeOnlyOne l1 l2 i1 i2=
+mergeSteps : List comparable -> List comparable -> Int -> Int -> (List comparable, List (List comparable), List (Int, Int))
+mergeSteps l1 l2 i1 i2=
     case ( l1, l2 ) of
         ( [], _ ) -> (l2, [], [])
         ( _, [] ) -> (l1, [], [])
         ( e1 :: r1, e2 :: r2 ) ->
             if e1 < e2 then
                 let
-                    (l, res, seq) = mergeOnlyOne r1 (e2 :: r2) (i1+1) i2
+                    (l, res, seq) = mergeSteps r1 (e2 :: r2) (i1+1) i2
                 in 
                 (e1 :: l,
                  (e1 :: r1 ++ e2 :: r2) :: List.map (\a -> e1 :: a) res,
@@ -66,7 +66,7 @@ mergeOnlyOne l1 l2 i1 i2=
                  )
             else
                 let
-                    (l, res, seq) = mergeOnlyOne (e1 :: r1) r2 (i1+1) (i2+1)
+                    (l, res, seq) = mergeSteps (e1 :: r1) r2 (i1+1) (i2+1)
                 in 
                 (e2 :: l,
                  (e2 :: (e1 :: r1) ++ r2) :: List.map (\a -> e2 :: a) res ,
