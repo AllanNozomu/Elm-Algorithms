b8ac5b5e87f271fab6bc85143b999572f3442b6e
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2020-06-02 21:49:37 -0300

    Add selection sort test

---
+++ tests/SelectionSortTest.elm
@@ -0,0 +1,27 @@
+module SelectionSortTest exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (int, list, string)
+import SelectionSort exposing (selectionSort)
+import Test exposing (..)
+
+suite : Test
+suite =
+    describe "Selection sort test"
+        [ test "Empty list" <|
+            \_ ->
+                Expect.equal [] (selectionSort [])
+        , test "Single element list" <|
+            \_ ->
+                Expect.equal [ 1 ] (selectionSort [ 1 ])
+        , fuzz (list int) "List of ints" <|
+            \randomList ->
+                randomList
+                    |> List.sort
+                    |> Expect.equal (selectionSort randomList)
+        , fuzz (list string) "List of strings" <|
+            \randomList ->
+                randomList
+                    |> List.sort
+                    |> Expect.equal (selectionSort randomList)
+        ]
