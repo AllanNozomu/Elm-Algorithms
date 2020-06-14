6491df5afa1abcb23b5f72de3df8daf63b94ca30
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2020-06-14 00:06:31 -0300

    Update only url if it really change

--- src/Update.elm
+++ src/Update.elm
@@ -1,55 +1,69 @@
-module Update exposing (update, Msg(..), SubPageMsg(..), changeRouteTo, init)
+module Update exposing (Msg(..), SubPageMsg(..), changeRouteTo, init, update)
 
+import Algorithms.Visualization.Model as Algorithms
+import Algorithms.Visualization.Update as Algorithms
 import Browser
 import Browser.Navigation as Nav
-import Url
+import Model exposing (CurrentModel(..), Model)
 import Random
-import Model exposing (Model, CurrentModel(..))
-import Algorithms.Visualization.Update as Algorithms
-import Algorithms.Visualization.Model as Algorithms
 import Route exposing (Route(..))
+import Url
+
 
 type Msg
-    = 
-    LinkClicked Browser.UrlRequest
+    = LinkClicked Browser.UrlRequest
     | UrlChanged Url.Url
     | SubPageMsg SubPageMsg
 
-type SubPageMsg 
+type SubPageMsg
     = AlgorithmsMsg Algorithms.Msg
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init _ url key =
     changeRouteTo (Route.fromUrl url) (Model.initModel url key)
 
+
 changeRouteTo : Maybe Route -> Model -> ( Model, Cmd Msg )
-changeRouteTo url model  =
+changeRouteTo url model =
     case url of
-        Just Home -> ({ model | currentModel = HomeModel }, Cmd.none)
-        Just (SortAlgorithmsPage _) -> ( { model | currentModel = SortAlgorithmsModel Algorithms.initModel },
-         List.map (\s -> Cmd.map AlgorithmsMsg s |> Cmd.map SubPageMsg) [Random.generate Algorithms.NewSeedStart (Random.int 1 1000000)]  |> Cmd.batch)
-        _ -> (model, Cmd.none)
+        Just Home ->
+            ( { model | currentModel = HomeModel }, Cmd.none )
+
+        Just (SortAlgorithmsPage _) ->
+            ( { model | currentModel = SortAlgorithmsModel Algorithms.initModel }
+            , List.map (\s -> Cmd.map AlgorithmsMsg s |> Cmd.map SubPageMsg) [ Random.generate Algorithms.NewSeedStart (Random.int 1 1000000) ] |> Cmd.batch
+            )
+
+        _ ->
+            ( model, Cmd.none )
+
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case (msg, model) of
-        (UrlChanged url, _) -> 
-            changeRouteTo (Route.fromUrl url) model
-
-        (LinkClicked urlRequest, _) ->
-            case urlRequest of
-                Browser.Internal url -> 
-                    ( model, Nav.pushUrl model.key (Url.toString url))
-                Browser.External href ->
-                    (model, Nav.load href)
-
-        (SubPageMsg subPageMsg, _) ->
-            case (subPageMsg, model.currentModel) of
-                (AlgorithmsMsg algortihmMsg, SortAlgorithmsModel subModel) ->
-                    let
-                        (newModel, subCmd) = Algorithms.update algortihmMsg subModel
-                    in
-                        ({model | currentModel = SortAlgorithmsModel newModel}, Cmd.map SubPageMsg (Cmd.map AlgorithmsMsg subCmd))
+    case ( msg, model ) of
+        ( UrlChanged url, _ ) ->
+            if url == model.url then
+                ( model, Cmd.none )
+            else 
+                changeRouteTo (Route.fromUrl url) {model | url = url}
                 
-                (_, HomeModel) ->
-                    (model, Cmd.none)
+
+        ( LinkClicked urlRequest, _ ) ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        ( SubPageMsg subPageMsg, _ ) ->
+            case ( subPageMsg, model.currentModel ) of
+                ( AlgorithmsMsg algortihmMsg, SortAlgorithmsModel subModel ) ->
+                    let
+                        ( newModel, subCmd ) =
+                            Algorithms.update algortihmMsg subModel
+                    in
+                    ( { model | currentModel = SortAlgorithmsModel newModel }, Cmd.map SubPageMsg (Cmd.map AlgorithmsMsg subCmd) )
+
+                ( _, HomeModel ) ->
+                    ( model, Cmd.none )
