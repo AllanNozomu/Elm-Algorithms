6864ec45d3e173cba57ae4f8936f4f2b66a13662
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2020-06-14 00:06:41 -0300

    Add navbar

--- src/View.elm
+++ src/View.elm
@@ -3,10 +3,14 @@ module View exposing (view)
 import Algorithms.Visualization.SortView as SortView
 import Browser
 import Html.Styled as Html exposing (..)
-import Html.Styled.Attributes exposing (class)
+import Html.Styled.Attributes as HtmlAttributes exposing (class, id, disabled)
 import Model exposing (CurrentModel(..), Model)
 import Pages.Home as Home
+import Route exposing (Route(..), href)
 import Update exposing (Msg(..), SubPageMsg(..))
+import VirtualDom.Styled exposing (attribute)
+import Html.Styled.Events exposing (onClick)
+import Url
 
 
 view : Model -> Browser.Document Msg
@@ -24,7 +28,36 @@ view model =
     in
     { title = "Elm algorithms"
     , body =
-        [ div [ class "container" ]
+        [ navbar model.url
+        , div [ class "container" ]
             [ currentPage ]
-        ] |> List.map Html.toUnstyled
+        ]
+            |> List.map Html.toUnstyled
     }
+
+
+navbar : Url.Url -> Html Msg
+navbar url =
+    nav [ class "navbar navbar-expand-lg navbar-light bg-light" ]
+        [ a [ class "navbar-brand" ] [ text "Algorithms in Elm" ]
+        , div [ class "collapse navbar-collapse" ]
+            [ ul [ class "navbar-nav" ]
+                [ li [ class "nav-item dropdown" ]
+                    [ a
+                        [ class "nav-link dropdown-toggle"
+                        , HtmlAttributes.href <| "#" ++ Maybe.withDefault "" url.fragment
+                        , attribute "data-toggle" "dropdown"
+                        ]
+                        [ text "Sort Algorithms" ]
+                    , div [ class "dropdown-menu" ]
+                        [ a [ class "dropdown-item", href (Route.SortAlgorithmsPage "mergeSort") ] [ text "Merge Sort" ]
+                        -- , a [ class "dropdown-item", href (Route.SortAlgorithmsPage "selectionSort") ] [ text "Merge Sort" ]
+                        ]
+                    ]
+                , li [ class "nav-item"]
+                    [ a [ class "nav-link disabled"]
+                        [ text "Under construction" ]
+                    ]
+                ]
+            ]
+        ]
