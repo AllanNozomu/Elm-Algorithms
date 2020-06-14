3674b521b78c018b487fe41aeebe1fa289cb0f51
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2020-06-14 19:57:16 -0300

    Fix right pointer not correct at merge sort

--- src/Pages/Sort/Algorithms/MergeSort.elm
+++ src/Pages/Sort/Algorithms/MergeSort.elm
@@ -17,7 +17,7 @@ mergeSortStepsAux l i =
             case (mergeSortStepsAux left i , mergeSortStepsAux right (i + List.length left)) of 
             ((sortedLeft, resLeft, sequenceLeft), (sortedRight, resRight, sequenceRight)) ->
                 let
-                    (sortedL, res, sequence) = mergeSteps sortedLeft sortedRight i (i + List.length left + 1)
+                    (sortedL, res, sequence) = mergeSteps sortedLeft sortedRight i (i + List.length left)
                 in  
                     (sortedL, 
                     List.map (\ele -> ele ++ right) resLeft ++ List.map (\ele -> sortedLeft ++ ele) resRight ++ res,
@@ -44,5 +44,5 @@ mergeSteps l1 l2 i1 i2=
                 in 
                 (e2 :: l,
                  (e2 :: (e1 :: r1) ++ r2) :: List.map (\a -> e2 :: a) res ,
-                 (i1, i2) :: seq
+                 (i1, i2 + 1) :: seq
                 )
