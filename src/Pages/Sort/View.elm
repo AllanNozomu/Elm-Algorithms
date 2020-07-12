cfb0863f4ee451f497632bc25df9b384de5e2c55
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2020-07-12 15:53:32 -0300

    Change to use canvas

--- src/Pages/Sort/View.elm
+++ src/Pages/Sort/View.elm
@@ -3,62 +3,60 @@ module Pages.Sort.View exposing (view)
 import Array
 import Css exposing (..)
 import FeatherIcons
-import Html.Styled exposing (..)
+import Canvas exposing (..)
+import Canvas.Settings exposing (..)
+import Color
+import Html.Styled as Html exposing (..)
 import Html.Styled.Attributes as HtmlAttributes exposing (attribute, class, css, max, min, step, type_, value)
 import Html.Styled.Events exposing (onClick, onInput)
 import Html.Styled.Keyed as HtmlKeyed
 import Html.Styled.Lazy as HtmlLazy
 import Pages.Sort.Model exposing (Model, SortType(..))
 import Pages.Sort.Update exposing (Msg(..))
-import Svg.Styled as Svg
-import Svg.Styled.Attributes as SvgAttrs
-import Svg.Styled.Keyed as SvgKeyed
-import Svg.Styled.Lazy as SvgLazy
 import Utils.IconUtils exposing (toStyledHtml)
 
 
-svgRect2 : Int -> Int -> Int -> Int -> Int -> Svg.Svg Msg
-svgRect2 index barHeight left right qty =
+whiteColorAttr =
+    Canvas.Settings.fill (Color.rgb 1 1 1)
+
+getColor i bh l r =
+    if i == l then
+        Canvas.Settings.fill (Color.rgb 1 0 0)
+
+    else if i == r then
+        Canvas.Settings.fill (Color.rgb 1 1 0)
+
+    else if i == bh then
+        Canvas.Settings.fill (Color.rgb 0 0.8 0)
+
+    else
+        Canvas.Settings.fill (Color.rgb 0 0 0)
+
+drawRects listToBeSorted left right =
     let
+        qty = List.length listToBeSorted |> Basics.toFloat
         h =
-            512 // qty
+            256 / qty
 
         w =
-            1024 // qty
+            512 / qty
 
-        getColor i bh l r =
-            if i == l then
-                SvgAttrs.fill "red"
-
-            else if i == r then
-                SvgAttrs.fill "yellow"
-
-            else if i == bh then
-                SvgAttrs.fill "green"
-
-            else
-                SvgAttrs.fill "black"
     in
-    Svg.rect
-        [ SvgAttrs.x <| String.fromInt <| (index * w)
-        , SvgAttrs.y <| String.fromInt (512 - (barHeight + 1) * h)
-        , SvgAttrs.width <| String.fromInt w
-        , SvgAttrs.height <| String.fromInt <| (barHeight + 1) * h
-        , getColor index barHeight left right
+    List.indexedMap(\index barHeight ->
+        let
+            floatBarHeight = Basics.toFloat barHeight
+            floatIndex = Basics.toFloat index
+        in
+        shapes [getColor index barHeight left right][
+            rect (floatIndex * w, 256 - (floatBarHeight + 1) * h) w ((floatBarHeight + 1) * h)
         ]
-        []
-
-
-svgRect : Int -> Int -> Int -> ( Int, Int ) -> ( String, Svg.Svg Msg )
-svgRect index barHeight qty ( currentLeft, currentRight ) =
-    ( String.fromInt index, SvgLazy.lazy5 svgRect2 index barHeight currentLeft currentRight qty )
-
+    ) listToBeSorted
 
 showCode : String -> ( String, Html Msg )
 showCode strCode =
     let
         f x =
-            code [ class "elm" ] [ text x ]
+            code [ class "elm" ] [ Html.text x ]
     in
     ( strCode, HtmlLazy.lazy f strCode )
 
@@ -66,12 +64,12 @@ showCode strCode =
 view : Model -> Html Msg
 view model =
     let
-        listToBeSorted =
+        currentStep =
             if Array.isEmpty model.steps then
-                Array.fromList model.listToBeSorted
+                model.listToBeSorted
 
             else
-                model.currentStep
+                Array.toList model.currentStep
 
         { sortInfo } =
             model
@@ -80,8 +78,8 @@ view model =
         [ div [ class "row" ]
             [ div [ class "col" ]
                 [ h1 []
-                    [ text <| sortInfo.sortName ]
-                , h2 [] [ text <| String.fromInt model.listLength ++ " Elements" ]
+                    [ Html.text <| sortInfo.sortName ]
+                , h2 [] [ Html.text <| String.fromInt model.listLength ++ " Elements" ]
                 , input
                     [ type_ "range"
                     , class "form-control-range"
@@ -93,7 +91,7 @@ view model =
                     ]
                     []
                 , h2 []
-                    [ text <| String.fromInt model.index ++ " Steps "
+                    [ Html.text <| String.fromInt model.index ++ " Steps "
                     , span
                         [ attribute "data-toggle" "tooltip"
                         , attribute "data-placement" "right"
@@ -110,36 +108,32 @@ view model =
                         , justifyContent center
                         , margin (px 5)
                         , width (pct 100)
-                        ]
+                        ],
+                        HtmlAttributes.id "teste"
                     ]
-                    [ SvgKeyed.node "svg"
-                        [ SvgAttrs.width "100%"
-                        , SvgAttrs.height "100%"
-                        , SvgAttrs.viewBox "0 0 1024 512"
-                        ]
-                        (Array.indexedMap
-                            (\index barHeight ->
-                                svgRect index barHeight (Array.length listToBeSorted) ( model.currentLeft, model.currentRight )
+                    [ Html.fromUnstyled <|
+                        Canvas.toHtml ( 512, 256 )
+                            []
+                            (shapes [ whiteColorAttr ]
+                                [ rect ( 0, 0 ) 512 256 ] ::
+                                drawRects currentStep model.currentLeft model.currentRight
                             )
-                            listToBeSorted
-                            |> Array.toList
-                        )
                     ]
                 ]
             ]
         , div [ class "row" ]
             [ div [ class "col" ]
-                [ button [ onClick Shuffle, class "btn mx-1 btn-dark" ] [ text "Shuffle" ]
-                , button [ onClick Back, HtmlAttributes.disabled (model.index <= 0), class "btn mx-1 btn-dark" ] [ text "<" ]
-                , button [ onClick Pause, HtmlAttributes.disabled model.pause, class "btn mx-1 btn-dark" ] [ text "Pause" ]
-                , button [ onClick Continue, HtmlAttributes.disabled <| not model.pause, class "btn mx-1 btn-dark" ] [ text "Continue" ]
-                , button [ onClick Advance, HtmlAttributes.disabled (model.index >= Array.length model.steps || Array.isEmpty model.steps), class "btn mx-1 btn-dark" ] [ text ">" ]
+                [ button [ onClick Shuffle, class "btn mx-1 btn-dark" ] [ Html.text "Shuffle" ]
+                , button [ onClick Back, HtmlAttributes.disabled (model.index <= 0), class "btn mx-1 btn-dark" ] [ Html.text "<" ]
+                , button [ onClick Pause, HtmlAttributes.disabled model.pause, class "btn mx-1 btn-dark" ] [ Html.text "Pause" ]
+                , button [ onClick Continue, HtmlAttributes.disabled <| not model.pause, class "btn mx-1 btn-dark" ] [ Html.text "Continue" ]
+                , button [ onClick Advance, HtmlAttributes.disabled (model.index >= Array.length model.steps || Array.isEmpty model.steps), class "btn mx-1 btn-dark" ] [ Html.text ">" ]
                 ]
             ]
         , div [ class "row" ]
             [ div [ class "col" ]
                 [ h2 []
-                    [ text "Code "
+                    [ Html.text "Code "
                     , span
                         [ attribute "data-toggle" "tooltip"
                         , attribute "data-placement" "right"
