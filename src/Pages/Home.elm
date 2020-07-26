2809358de2da182b0e451fb62ce0e97bfd7b2f60
Allan Nozomu Fukasawa <allannozomu@gmail.com>
2020-07-26 20:25:08 -0300

    Add algorithms to home page

--- src/Pages/Home.elm
+++ src/Pages/Home.elm
@@ -4,14 +4,25 @@ import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (class)
 import Route exposing (Route(..), href)
 import Update exposing (Msg(..))
+import Pages.Sort.Model as SortModel
+import Dict
 
 
 view : Html Msg
 view =
+    let
+        sortMethods =
+            Dict.toList SortModel.sortInfos
+            |> List.map (\(_, {sortName}) -> sortName) 
+    in
     div [ class "row" ]
         [ div [class "col"]
             [ h1 []
                 [ text "Algorithms in elm" ]
-            , a [ href (Route.SortAlgorithmsPage "mergeSort") ] [ text "Sort algorithms" ]
+            , h3 [][text "Sort Algorithms"]
+            , ul [] (List.map (\sm ->
+                li [][ a [ href (Route.SortAlgorithmsPage sm) ] [ text sm ] ])
+                sortMethods
+                )
             ]
         ]
